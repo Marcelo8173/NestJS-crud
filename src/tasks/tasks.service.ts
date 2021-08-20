4f92262d42bf18b05b85b900e50855c66b3b9cc1
Marcelo nascimento <marcelo.nascimento@skyone.solutions>
2021-08-19 22:53:52 -0300

    usando o typeOrm para persistir no banco

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,37 +1,60 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import {ITasks,TasksStatus} from './tasks.model'
+import {TasksStatus} from './tasks.enum'
 import { v4 as uuid } from 'uuid'
 import { CreateTasksDto } from './DTOSTasks/create-tasks.dto';
+import { TasksRepository } from './tasks.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tasks } from './tasks.entity';
 
 @Injectable()
 export class TasksService {
-    private tasks:ITasks[]  = []
 
-    public getAllTasks():ITasks[]{
-        return this.tasks
+    constructor(
+        @InjectRepository(TasksRepository)
+        private tasksRepository:TasksRepository
+    ){}
+
+
+    public async getAllTasks():Promise<Tasks[]>{
+        return await this.tasksRepository.find();
     }
 
-    public getRaksId(id:string):ITasks{
-        const taksId = this.tasks.find(item => item.id === id)
-        
-        if(!taksId){
+    public async getRaksId(id:string):Promise<Tasks>{
+        const found = await this.tasksRepository.findOne({
+            where: id
+        })
+
+        if(!found){
             throw new NotFoundException(`Tasks id with ${id} not found`);
         }
 
-        return taksId
+        return found;
     }
 
-    public createTasksService(createTasksDto:CreateTasksDto):ITasks{
-
+    public async createTasksService(createTasksDto:CreateTasksDto):Promise<Tasks>{
         const {title,description} = createTasksDto
-
-        const tasks: ITasks = {
+        const tasks: Tasks = {
             id: uuid(),
             title,
             description,
             status: TasksStatus.DONE
         }
-        this.tasks.push(tasks)
-        return tasks
+        
+        const created = this.tasksRepository.create(tasks)
+
+        return await this.tasksRepository.save(created)
     }
+    // public createTasksService(createTasksDto:CreateTasksDto):ITasks{
+
+    //     const {title,description} = createTasksDto
+
+    //     const tasks: ITasks = {
+    //         id: uuid(),
+    //         title,
+    //         description,
+    //         status: TasksStatus.DONE
+    //     }
+    //     this.tasks.push(tasks)
+    //     return tasks
+    // }
 }
